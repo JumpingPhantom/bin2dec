d255fcda30cb78cd31289427cce2d80b6239b3c9
JumpingPhantom <104843253+jumpingphantom@users.noreply.github.com>
2024-02-25 18:56:54 +0300

    Added conversion functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bin2dec';
+  binaryInput: string = '0';
+  result: string = '0';
+
+  handleInputChange(event: any) {
+    this.binaryInput = event.target.value || '0';
+    this.result = this.convertToDecimal(this.binaryInput);
+  }
+
+  convertToDecimal(bin: string) {
+    return parseInt(bin, 2).toString();
+  }
 }
